0f99c365453833b74b59752c8dcda138e2caacc6
smwikipedia <smwikipedia@163.com>
2025-07-01 21:33:18 +0800

    Some experiment materials
    Signed-off-by: smwikipedia <smwikipedia@163.com>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SM_site'
+project = 'SM_site #1'
 copyright = '2025, smwikipedia'
 author = 'smwikipedia'
 
@@ -26,10 +26,12 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+html_sidebars = {
+    "**": ['sbt-sidebar-nav.html']
+}
---
+++ sphinx/index.md
@@ -0,0 +1,42 @@
+# This is the index.md!
+## Hello, Sphinx Markdown!
+### title 3
+#### title 4
+
+```{toctree}
+:maxdepth: 1
+:caption: "Contents:"
+
+page1
+```
+
+```c
+void func1(void)
+{
+    return;
+}
+```
+
+Investors say the best things about us:
+
+> I'm so glad I put my life savings in Schlockchain.
+>
+> > Said Nobody Ever
+
+
+We have a patented algorithm:
+
+- Glacially slow
+- Which melts glaciers
+
+...and a roadmap to success:
+
+1. Start an LLC
+2. Get a Series A investment round
+3. Get a Series B investment round
+4. Continue
+
+
+![Python Logo](./_static/python_logo.png)
+
+a formula:  $y = F(x) =  \int_1^x\frac{1}{t}dt$
